7dd40a3021190a22a2937d0eeacaf67255297e13
Peter Massey-Plantinga <plantinga.peter@gmail.com>
2013-05-25 11:40:08 -0700

    Don't include style.css twice

--- header.php
+++ header.php
@@ -4,7 +4,6 @@
 		<title><?php wp_title( '| ', TRUE, 'right' ); bloginfo( 'name' ); ?></title>
 		<meta charset="<?php bloginfo( 'charset' ); ?>">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
-		<link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_directory_uri() . "/style.css"; ?>" />
 		<!--[if lt IE 9]>
 		<script src="<?php echo get_template_directory_uri(); ?>/html5.js" type="text/javascript"></script>
 		<![endif]-->
