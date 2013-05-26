ffec486a085ca69a94fa5ce2259dcfd08c09ae9b
Peter Massey-Plantinga <plantinga.peter@gmail.com>
2013-05-26 09:13:51 -0700

    Add conditional two-level menu

--- functions.php
+++ functions.php
@@ -95,3 +95,69 @@ function themage_body_class( $classes )
 }
 add_filter( 'body_class', 'themage_body_class' );
 
+/**
+ * This function copied from the "two level conditional menu" plugin
+ * that I authored. Here is version 1.1
+ *
+ * To override, remove_filter( 'two_level_nav_menu' );
+ */
+function two_level_nav_menu( $menu ) {
+	$end_div = substr( $menu, -6 ) == '</div>';
+
+	// Remove the second-level menus
+	$menu = preg_replace( '/<ul class="[^"]*sub-menu[^"]*">.*?<\/ul>/s', '', $menu );
+
+	// Trim ending </div>
+	$output = preg_replace( '/<\/div>$/', '', $menu );
+
+	// Add second level menu
+	global $post;
+	if ( $post ) {
+
+		// Gather current menu items
+		$locations = get_nav_menu_locations();
+		$menu_items = wp_get_nav_menu_items( $locations[ 'primary' ] );
+
+		$second_level = "";
+		if ( ! empty( $menu_items ) ) {
+
+			// First, find parent id, if it exists
+			$parent_id = -1;
+			$current_page_id = is_home() ? get_option( 'page_for_posts' ) : get_the_ID();
+			foreach ( $menu_items as $item ) {
+				if ( $item->object_id == $current_page_id ) {
+					if ( $item->menu_item_parent == 0 ) {
+						$parent_id = $item->ID;
+					} else {
+						$parent_id = $item->menu_item_parent;
+					}
+					break;
+				}
+			}
+
+			// Now add menu items
+			foreach ( $menu_items as $item ) {
+
+				// Figure out if we're on the current page
+				$class = '';
+				if ( $item->object_id == $current_page_id )
+					$class = ' class="current-menu-item"';
+				
+				// add menu item
+				if ( $item->menu_item_parent == $parent_id )
+					$second_level .= "<li$class><a href='$item->url'>$item->title</a></li>";
+			}
+		}
+
+		if ( ! empty( $second_level ) )
+			$output .= "<ul class='second-level menu'>$second_level</ul>";
+	}
+
+	// Re-add ending div
+	if ( $end_div )
+		$output .= "</div>";
+
+	return $output;
+}
+add_filter( 'wp_nav_menu', 'two_level_nav_menu' );
+
