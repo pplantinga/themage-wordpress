5e39912ead32191f1af3498d6bc20f79a5cb6e1e
Peter Massey-Plantinga <plantinga.peter@gmail.com>
2013-05-28 16:42:35 -0700

    Update conditional menu to version 1.2

--- functions.php
+++ functions.php
@@ -120,35 +120,60 @@ add_filter( 'body_class', 'themage_body_class' );
 
 /**
  * This function copied from the "two level conditional menu" plugin
- * that I authored. Here is version 1.1
+ * that I authored. Here is version 1.2
  *
  * To override, remove_filter( 'wp_nav_menu', 'themage_conditional_menu' );
  */
-function themage_conditional_menu( $menu ) {
-	$end_div = substr( $menu, -6 ) == '</div>';
+function themage_conditional_menu( $menu, $args ) {
 
-	// Remove the second-level menus
-	$menu = preg_replace( '/<ul class="[^"]*sub-menu[^"]*">.*?<\/ul>/s', '', $menu );
+  // Start output by removing the second-level menu
+  $output = preg_replace( '/<ul class="[^"]*sub-menu[^"]*">.*?<\/ul>/s', '', $menu );
 
-	// Trim ending </div>
-	$output = preg_replace( '/<\/div>$/', '', $menu );
+  // Remove endtag if exists
+  $endtag = "</$args->container>";
+  $endtag_exists = substr( $output, -strlen( $endtag ) ) == $endtag;
+  if ( $endtag_exists )
+    $output = substr( $output, 0, -strlen( $endtag ) );
 
 	// Add second level menu
 	global $post;
+	global $wp_query;
 	if ( $post ) {
+		/* This menu-gathering code is from wp_nav_menu */
+		// Get the nav menu based on the requested menu
+		$menu = wp_get_nav_menu_object( $args->menu );
 
-		// Gather current menu items
-		$locations = get_nav_menu_locations();
-		$menu_items = wp_get_nav_menu_items( $locations[ 'primary' ] );
+		// Get the nav menu based on the theme_location
+		if ( ! $menu && $args->theme_location && ( $locations = get_nav_menu_locations() ) && isset( $locations[ $args->theme_location ] ) )
+			$menu = wp_get_nav_menu_object( $locations[ $args->theme_location ] );
+
+		// get the first menu that has items if we still can't find a menu
+		if ( ! $menu && !$args->theme_location ) {
+			$menus = wp_get_nav_menus();
+			foreach ( $menus as $menu_maybe ) {
+				if ( $menu_items = wp_get_nav_menu_items( $menu_maybe->term_id, array( 'update_post_term_cache' => false ) ) ) {
+					$menu = $menu_maybe;
+					break;
+				}
+			}
+		}
+
+		// If the menu exists, get its items.
+		if ( $menu && ! is_wp_error( $menu ) && !isset( $menu_items ) )
+			$menu_items = wp_get_nav_menu_items( $menu->term_id, array( 'update_post_term_cache' => false ) );
 
 		$second_level = "";
-		if ( ! empty( $menu_items ) ) {
-
+		if ( ! empty( $menu_items ) )
+		{
 			// First, find parent id, if it exists
 			$parent_id = -1;
-			$current_page_id = is_home() ? get_option( 'page_for_posts' ) : get_the_ID();
+			$current_page_id = $wp_query->get_queried_object_ID();
 			foreach ( $menu_items as $item ) {
+
+				// Are we on the current page?
 				if ( $item->object_id == $current_page_id ) {
+
+					// Correctly set menu parent
 					if ( $item->menu_item_parent == 0 ) {
 						$parent_id = $item->ID;
 					} else {
@@ -176,13 +201,13 @@ function themage_conditional_menu( $menu ) {
 			$output .= "<ul class='second-level menu'>$second_level</ul>";
 	}
 
-	// Re-add ending div
-	if ( $end_div )
-		$output .= "</div>";
+	// Re-add ending tag
+	if ( $endtag_exists )
+		$output .= $endtag;
 
 	return $output;
 }
-add_filter( 'wp_nav_menu', 'themage_conditional_menu' );
+add_filter( 'wp_nav_menu', 'themage_conditional_menu', 10, 2 );
 
 /**
  * Navigation below lists
